0a2c1645659e0af24dd4994649fac21d0e60a104
julia-francais <ju.francais@gmail.com>
2020-03-17 14:08:59 +0100

    fetch posts added

---
+++ src/pages/home.js
@@ -0,0 +1,41 @@
+import React, { Component } from "react";
+import axios from "axios";
+import Grid from "@material-ui/core/Grid";
+
+import Post from "../components/Post";
+
+export class Home extends Component {
+  state = {
+    posts: null
+  };
+  componentDidMount() {
+    axios
+      .get("/screams")
+      .then(res => {
+        console.log(res.data);
+        this.setState({
+          posts: res.data
+        });
+      })
+      .catch(err => console.log(err));
+  }
+  render() {
+    let recentPostsMarkup = this.state.posts ? (
+      this.state.posts.map(post => <Post post={post} />)
+    ) : (
+      <p> Loading ...</p>
+    );
+    return (
+      <Grid container spacing={2}>
+        <Grid item sm={8} xs={12}>
+          {recentPostsMarkup}
+        </Grid>
+        <Grid item sm={4} xs={12}>
+          <p> Profile</p>
+        </Grid>
+      </Grid>
+    );
+  }
+}
+
+export default Home;
